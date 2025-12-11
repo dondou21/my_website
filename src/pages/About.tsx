990a2246bfedbfb9cf5255bffa33ae7395f75256
dondou21 <dondouabiyi@gmail.com>
2025-12-11 11:15:02 +0200

    Style of new about page done

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -35,13 +35,51 @@ const About = () => {
   ];
 
   return (
-    <section>
-      <h2>My Story</h2>
-
+    <section className="py-16">
+      
       <div className="relative">
-        <div className="ablsolute left-1/2 transform -translate-x-1/2 h-full w-[3px] bg-blue-700"></div>
-        <div>
+        <h2 className="text-3xl font-bold text-center mb-2 py-10">My Journey</h2>
+          <div className="h-1 w-20 bg-blue-600 mx-auto mb-8"></div>
+          {/* Vertical line */}
+        <div className="absolute left-1/2 transform -translate-x-1/2 h-full w-[3px] bg-blue-700"></div>
+        <div className="space-y-16 ">
+          {events.map((event, index) => (
+            <div 
+             key={index}
+             className={`relative flex items-center 
+              ${event.position === "left" ? "justify-start":"justify-end"}
+              `}
+            >
+              <div className={`w-[45%] bg-white shadow-md p-6 rounded-xl border`}>
+                <h3 className="font-bold text-lg mb-2 text-blue-800">{event.year}</h3>
+                <p className="font-semibold mb-3 text-green-600 text-xl">{event.title}</p>
+                <p className="text-gray-600">{event.descriotion}</p>
+              </div>
+               <div className="absolute flex font-bold text-xl text-green-600 left-1/2 transform -translate-x-1/2 w-10 h-10 bg-white border-4 border-green-500 rounded-full justify-center">{index}</div>
+            </div>
+          ))}
+        </div>
+      </div>
 
+       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-3xl mx-auto">
+           <h2 className="text-3xl font-bold text-center mb-2 pt-48">About Me</h2>
+          <div className="h-1 w-20 bg-blue-600 mx-auto mb-8"></div>
+          <div className="bg-white rounded-lg shadow-md p-8">
+            <p className="text-gray-700 leading-relaxed mb-6">
+              Creative and adaptable UI/UX & Graphics Designer with a strong
+              foundation in software engineering and a passion for user-centered
+              design. Skilled in creating intuitive interfaces with Figma,
+              collaborating closely with development teams, and delivering
+              consistent, brand-aligned visuals.
+            </p>
+            <p className="text-gray-700 leading-relaxed">
+              Known for integrity, bold ideas, and a strong sense of
+              responsibility to deliver excellence across platforms. I combine
+              technical expertise with design sensibility to create engaging
+              digital experiences that are both beautiful and functional.
+            </p>
+          </div>
         </div>
       </div>
     </section>
